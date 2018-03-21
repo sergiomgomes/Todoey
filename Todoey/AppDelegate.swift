7666bf395d0ee05a96661bf2b4eee459ece2fbc0
Sergio Gomes (1130399) <1130399@isep.ipp.pt>
2018-03-21 20:52:24 +0000

    Implemented todo items persistence using PList's, encoding and decoding the data

--- Todoey/AppDelegate.swift
+++ Todoey/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         //Get Local sandbox with plist that contains the data
-        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
+        //print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
         
         return true
     }
--- Todoey/Controllers/TodoTableViewController.swift
+++ Todoey/Controllers/TodoTableViewController.swift
@@ -12,10 +12,12 @@ class TodoTableViewController: UITableViewController {
 
     var todoItems = [Item]()
     
-    let defaults = UserDefaults.standard
+    let dataFilePath =  FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("TodoItems.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        loadTodoItems()
     }
     
     //MARK - Tableview Datasource Methods
@@ -40,7 +42,7 @@ class TodoTableViewController: UITableViewController {
         
         todoItems[indexPath.row].done = !todoItems[indexPath.row].done
         
-        tableView.reloadData()
+        saveTodoItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -58,8 +60,8 @@ class TodoTableViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (actionResult) in
             
             self.todoItems.append(Item(title: textField.text! != "" ? textField.text! : "New Item", done: false))
-            
-            self.tableView.reloadData()
+
+            self.saveTodoItems()
         }
         
         alert.addTextField { (alertTextField) in
@@ -69,6 +71,31 @@ class TodoTableViewController: UITableViewController {
         
         alert.addAction(action)
         self.present(alert, animated: true, completion: nil)
+    }
+    
+    func saveTodoItems(){
+        let encoder = PropertyListEncoder()
+        
+        do{
+            let data = try encoder.encode(self.todoItems)
+            try data.write(to: self.dataFilePath!)
+        }catch{
+            print("ERROR encoding todo items array, \(error)")
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func loadTodoItems(){
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            
+            do{
+                self.todoItems = try decoder.decode([Item].self, from: data)
+            }catch{
+                 print("ERROR decoding todo items array, \(error)")
+            }
+        }
     }
 }
 
